d58b265df8ac929c3b8c8afce68314268a4d8b83
EliRmz <elisaram@gmail.com>
2023-11-20 16:56:27 +0100

    MVP version

--- lib/cashier/cart.rb
+++ lib/cashier/cart.rb
@@ -1,20 +1,41 @@
 class Cashier::Cart
-  attr_accessor :product_list, :product_registry
-
   def initialize
     @product_list = []
     @product_registry = {
-      "GR1" => { name: "Green Tea", price: 3.11 },
-      "SR1" => { name: "Strawberries", price: 5.00 },
-      "CF1" => { name: "Coffee", price: 11.23 }
+      "GR1" => { name: "Green Tea", price: 311, discount: "2x1" },
+      "SR1" => { name: "Strawberries", price: 500, discount: "> 3" },
+      "CF1" => { name: "Coffee", price: 1123, discount: "3x2" }
     }
   end
 
   def total_price
+    (subtotal - discount) / 100.0
+  end
+
+  def subtotal
     @product_list.sum { |product| product[:price] }
   end
 
+  def discount
+    gr1_discount + sr1_discount + cf1_discount
+  end
+
   def add_product(product)
-    @product_list << product_registry[product]
+    @product_list << @product_registry[product]
+  end
+
+  def gr1_discount
+    number_of_products = @product_list.count { |product| product[:name] == "Green Tea" }
+    (number_of_products / 2).floor * 311
+  end
+
+  def sr1_discount
+    number_of_products = @product_list.count { |product| product[:name] == "Strawberries" }
+    (number_of_products / 3).floor * (50 * 3)
+  end
+
+  def cf1_discount
+    number_of_products = @product_list.count { |product| product[:name] == "Coffee" }
+    (number_of_products / 3).floor * 1123
   end
 end
--- spec/cashier/cart_spec.rb
+++ spec/cashier/cart_spec.rb
@@ -1,17 +1,43 @@
 RSpec.describe Cashier::Cart do
-  it "returns 0 when there is no products added" do
-    cart = Cashier::Cart.new
+  before(:each) do
+    @cart = Cashier::Cart.new
+  end
 
-    expect(cart.total_price).to eq(0)
+  it "returns 0 when there is no products added" do
+    expect(@cart.total_price).to eq 0
   end
 
   it "returns 19,34 when 3 products are added" do
-    cart = Cashier::Cart.new
+    @cart.add_product("GR1")
+    @cart.add_product("SR1")
+    @cart.add_product("CF1")
 
-    cart.add_product("GR1")
-    cart.add_product("SR1")
-    cart.add_product("CF1")
+    expect(@cart.total_price).to eq 19.34
+  end
 
-    expect(cart.total_price).to eq(19.34)
+  it "returs 3.11 when two GR1 are added" do
+    @cart.add_product("GR1")
+    @cart.add_product("GR1")
+
+    expect(@cart.total_price).to eq 3.11
+  end
+
+  it "returs 16.61 when SR1, SR1, GR1 and SR1 are added" do
+    @cart.add_product("SR1")
+    @cart.add_product("SR1")
+    @cart.add_product("GR1")
+    @cart.add_product("SR1")
+
+    expect(@cart.total_price).to eq 16.61
+  end
+
+  it "returs 30.57 when GR1, CF1, SR1, CF1 and CF1 are added" do
+    @cart.add_product("GR1")
+    @cart.add_product("CF1")
+    @cart.add_product("SR1")
+    @cart.add_product("CF1")
+    @cart.add_product("CF1")
+
+    expect(@cart.total_price).to eq 30.57
   end
 end
