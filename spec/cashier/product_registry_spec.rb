7801d644d8b8fb775e0b9fed2b1447f30d809a8c
EliRmz <elisaram@gmail.com>
2023-11-23 18:16:23 +0100

    Clean tests

--- spec/cashier/product_registry_spec.rb
+++ spec/cashier/product_registry_spec.rb
@@ -1,11 +1,5 @@
 module Cashier
   RSpec.describe ProductRegistry do
-    PRODUCTS = [
-      { product_code: "GR1", name: "Green Tea", price: 311, discount_code: "2x1" },
-      { product_code: "SR1", name: "Strawberries", price: 500, discount_code: ">3" },
-      { product_code: "CF1", name: "Coffee", price: 1123, discount_code: "3x2" }
-    ]
-
     it "send an error message when a wrong product is added" do
       expect { ProductRegistry.find("WWW") }.to raise_error(UnknownProduct, "Unknown product")
     end
--- spec/cashier_spec.rb
+++ spec/cashier_spec.rb
@@ -1,5 +1,2 @@
 RSpec.describe Cashier do
-  xit "does something useful" do
-    expect(false).to eq(true)
-  end
 end
